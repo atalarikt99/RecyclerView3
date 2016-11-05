da58030412d7af1bb2d4a80369c43eb1369f882b
taufan99 <atalarikt@gmail.com>
2016-11-05 22:36:37 +0700

    Initial commit RecyclerView List Card RecyclerView View Detail RecyclerView Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -24,6 +24,7 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.adapter.HotelAdapter;
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter
 {
     public static final String HOTEL = "hotel";
+    public static final int REQUEST_CODE_ADD = 88;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
 
@@ -45,9 +46,9 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                goAdd();
             }
         });
     }
@@ -98,6 +99,23 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goAdd()
+    {
+        startActivityForResult(new Intent(this,InputActivity.class), REQUEST_CODE_ADD);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK)
+        {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
